7bb868273759deac48690999f103326eae296195
Himanshu Tanwar <himanshu@nightfall.ai>
2023-09-10 20:17:06 +0530

    feat(sensor): accept cards as props

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Sensor from './Sensor';
 
+const Cards = ['Amitabh Bachchan', 'Shahrukh Khan', 'Madhuri Dixit', 'Dharmendra', 'Sunny Deol', 'Bobby Deol', 'Abhishek Bachchan', 'Rani Mukherji', 'Kajol']
+
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Sensor />
+      <Sensor cards={Cards} />
     </View>
   );
 }
